37e99135d3c0873b59af7a387e369e14c4b99851
bfla <bfla@umich.edu>
2014-01-28 09:52:21 -0500

    made git repo, added slim

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'abaf65cc5790d394147cff98f54902df56c13287a4d46aa144d02c6ee31b1e261970c4e187518d25276db3ef868ff51c6142a459a0cf43c2883491543221ffbf'
