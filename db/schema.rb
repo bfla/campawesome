6e56f85289071898ca50b1ed930d4a52ce39cdfa
bfla <bfla@umich.edu>
2014-01-29 18:12:05 -0500

    updated campground form with lat and lon, added breadcrumb nav and lat and lon to show view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140129195147) do
+ActiveRecord::Schema.define(version: 20140129211856) do
 
   create_table "campsites", force: true do |t|
     t.string   "name"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 20140129195147) do
     t.datetime "updated_at"
     t.boolean  "reservable"
     t.boolean  "walkin"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "address"
   end
 
   create_table "users", force: true do |t|
