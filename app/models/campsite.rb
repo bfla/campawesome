ce19a642a06aa89c12635ddd993d351e573c1b82
bfla <bfla@umich.edu>
2014-01-30 08:26:50 -0500

    trying to add search controller now

--- app/models/campsite.rb
+++ app/models/campsite.rb
@@ -1,4 +1,8 @@
 class Campsite < ActiveRecord::Base
+  # name:string, org:string, description:text, 
+  # state_id:integer, latititude:float, longitude:float
+  # res_phone:string, camp_phone:string, res_url:string, camp_url:string, 
+  # reservable:boolean, walkin:boolean
   belongs_to :state
   def state_name
     self.state.name if self.state.name?
--- app/models/state.rb
+++ app/models/state.rb
@@ -1,3 +1,4 @@
 class State < ActiveRecord::Base
+  # name:string
   has_many :campsites
 end
