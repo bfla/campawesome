88002c70c2624b51ca884f67779d15dc03d36465
bfla <bfla@umich.edu>
2014-02-14 15:52:31 -0500

    edited user model to add simple location

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,11 +30,14 @@ class User < ActiveRecord::Base
   end
 
   def self.new_with_session(params, session)
-      super.tap do |user|
-        if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-          user.email = data["email"] if user.email.blank?
-          user.first_name = data['first_name'] if user.first_name.blank?
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+        user.first_name = data['first_name'] if user.first_name.blank?
+        if !data['user_location'].blank?
+          user.location = data['user_location']
         end
       end
     end
+  end
 end
