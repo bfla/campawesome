112c4c2b0cc4ffce3967fcb42a110af74a83d143
bfla <bfla@umich.edu>
2014-02-15 11:43:52 -0500

    attempting to calculate campsite's avg rating when a new associated rating is saved

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -4,4 +4,14 @@ class Rating < ActiveRecord::Base
   belongs_to :review
   validates :user, presence: true
   validates :value, numericality: {greater_than_or_equal:1.0, less_than_or_equal_to:5.0}
+  after_save :store_campsite_avg
+
+  private
+    def store_campsite_avg
+      if self.ratable_type == "Campsite"
+        sum = 0
+        self.ratable.ratings.each {|rating| sum = sum + rating.value}
+        self.ratable.avg_rating = sum / self.ratable.ratings.size
+      end
+    end
 end
