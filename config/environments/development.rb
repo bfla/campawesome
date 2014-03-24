46774762a4f790da6610b40b28f88d6752400cc3
bfla <bfla@umich.edu>
2014-03-24 12:32:54 -0400

    configured mailer settings in config.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,8 @@ Blog::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  # Mail method
+  config.action_mailer.delivery_method = :test
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   # Devise says it needs us to set the following line:
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,17 @@ Blog::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:        "smtp.gmail.com",
+    port:           587,
+    domain:         "getcamphero.com",
+    authentication: "plain",
+    user_name:      "brian@getcamphero.com",
+    password:       "Witpomc2t",
+    enable_starttls_auto: true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
