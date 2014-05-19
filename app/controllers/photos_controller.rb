ac5d6326b5f32953444095b1c7f13e559e5bd255
bfla <bfla@umich.edu>
2014-05-19 14:38:46 -0400

    fixed broken city links to state page and optimized images better

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -55,7 +55,7 @@ class PhotosController < ApplicationController
 
     respond_to do |format|
       if @photo.save
-        format.html { redirect_to @photo, notice: 'Photo was successfully created.' }
+        format.html { redirect_to @photo, notice: 'Photo was successfully added.' }
         format.json { render action: 'show', status: :created, location: @photo }
       else
         format.html { render action: 'new' }
